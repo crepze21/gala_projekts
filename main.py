e66c231fc474af80b557fd79a646e3010a737785
crepze21 <131647157+crepze21@users.noreply.github.com>
2025-04-14 09:29:46 +0300

    A

--- main.py
+++ main.py
@@ -4,10 +4,30 @@ import sqlite3
 app = Flask(__name__)
 conn = sqlite3.connect("static/data.db")
 
+conn.execute("""CREATE TABLE IF NOT EXISTS speletaji(
+             id INTEGER,
+             vards TEXT,
+             uzvards TEXT,
+             steam_id TEXT,
+             komandas_id INTEGER,
+             PRIMARY KEY("id"),
+             FOREIGN KEY (komandas_id) REFERENCES kommandas (id)
+             ) """)
 
-@app.route("/")
+conn.execute("""CREATE TABLE IF NOT EXISTS kommandas(
+             id INTEGER,
+             nosaukums TEXT,
+             kapteina_steam_id,
+             PRIMARY KEY("id")
+             )""")
+
+@app.route("/", methods=["GET","POST"])
 def index():
     return render_template("index.html")
+
+@app.route("/regi")
+def regi():
+    return render_template("regi.html")
 
 #palaiž mājaslapu
 if __name__ == "__main__":

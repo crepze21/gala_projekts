38e9c1a0c72cd1408178c7e3256691f4c6a86567
crepze21 <131647157+crepze21@users.noreply.github.com>
2025-04-28 09:26:51 +0300

    not gut

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ conn.execute("""CREATE TABLE IF NOT EXISTS kommandas(
              id INTEGER,
              nosaukums TEXT,
              kapteina_steam_id TEXT,
+             turnirs TEXT,
              PRIMARY KEY("id")
              )""")
-
+conn.commit()
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -33,22 +34,38 @@ def registration():
     nosaukums = request.form.get("nosaukums")
     name_kaptain = request.form.get("name_kaptain")
     steam_id_kaptain = request.form.get("steam_id_kaptain")
+
     name_speletajs_1 = request.form.get("name_speletajs_1")
     name_speletajs_2 = request.form.get("name_speletajs_2")
     name_speletajs_3 = request.form.get("name_speletajs_3")
     name_speletajs_4 = request.form.get("name_speletajs_4")
+
     steam_id_1 = request.form.get("steam_id_1")
     steam_id_2 = request.form.get("steam_id_2")
     steam_id_3 = request.form.get("steam_id_3")
     steam_id_4 = request.form.get("steam_id_4")
-    kommanda = [nosaukums, name_kaptain, steam_id_kaptain, name_speletajs_1, name_speletajs_2, name_speletajs_3, name_speletajs_4, steam_id_1,steam_id_2,steam_id_3,steam_id_4]
+
+    speletajs_kapteinis = [name_kaptain, steam_id_kaptain]
+    speletajs_1 = [name_speletajs_1, steam_id_1]
+    speletajs_2 = [name_speletajs_2, steam_id_2]
+    speletajs_3 = [name_speletajs_3, steam_id_3]
+    speletajs_4 = [name_speletajs_4, steam_id_4]
+    kommanda = [nosaukums, speletajs_kapteinis, speletajs_1, speletajs_2, speletajs_3, speletajs_4]
+
+    conn.execute('INSERT INTO kommandas(nosaukums, kapteina_steam_id) VALUES (?,?)',(nosaukums, steam_id_kaptain))
+    conn.commit()
     
+    kommandas_id = conn.execute(f"SELECT id FROM kommandas WHERE nosaukums = {nosaukums} ")
+    conn.execute('INSERT INTO speletaji(vards, steam_id, kommandas_id) VALUES (?,?,?,?)',(speletajs_1[0],speletajs_1[1], kommandas_id))
+    conn.commit()
+
     for i in kommanda:
-        if kommanda == "" or None:
+        if i == "" or None:
             kluda = True
             break
         else:
             kluda = False
+            
 
     return render_template("registration.html", kluda=kluda)
 

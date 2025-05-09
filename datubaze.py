262d00050f7501bd227388ed16c9163d9720cc8f
crepze21 <creepedude@gmail.com>
2025-05-09 07:33:09 +0300

    FACK FAKLCFAF

---
+++ datubaze.py
@@ -0,0 +1,191 @@
+import sqlite3
+
+
+class kommanda():
+
+    def __init__(self,nosaukums,kapteina_steam_nosaukums, kapteina_vards, kapteina_uzvards, speletaja_1_steam_nosaukums, speletaja_1_vards, speletaja_1_uzvards, speletaja_2_steam_nosaukums, speletaja_2_vards, speletaja_2_uzvards, speletaja_3_steam_nosaukums, speletaja_3_vards, speletaja_3_uzvards, speletaja_4_steam_nosaukums, speletaja_4_vards, speletaja_4_uzvards, turnira_nosaukums):
+        self.nosaukums = nosaukums
+
+        self.kaptaina_id = kapteina_steam_nosaukums
+        self.kapt_vards = kapteina_vards
+        self.kapt_uzvards = kapteina_uzvards
+
+        self.spl_1_id = speletaja_1_steam_nosaukums
+        self.spl_1_vards = speletaja_1_vards
+        self.spl_1_uzvards = speletaja_1_uzvards
+
+        self.spl_2_id = speletaja_2_steam_nosaukums
+        self.spl_2_vards = speletaja_2_vards
+        self.spl_2_uzvards = speletaja_2_uzvards
+
+        self.spl_3_id = speletaja_3_steam_nosaukums
+        self.spl_3_vards = speletaja_3_vards
+        self.spl_3_uzvards = speletaja_3_uzvards
+
+        self.spl_4_id = speletaja_4_steam_nosaukums
+        self.spl_4_vards = speletaja_4_vards
+        self.spl_4_uzvards = speletaja_4_uzvards
+
+        self.trn_nosaukums = turnira_nosaukums
+
+    def get_nosaukums(self):
+        return self.nosaukums
+    
+    def get_speletaji(self):
+        return [self.kaptaina_id,
+        self.kapt_vards ,
+        self.kapt_uzvards,
+
+        self.spl_1_id,
+        self.spl_1_vards,
+        self.spl_1_uzvards,
+
+        self.spl_2_id,
+        self.spl_2_vards,
+        self.spl_2_uzvards,
+
+        self.spl_3_id,
+        self.spl_3_vards,
+        self.spl_3_uzvards,
+
+        self.spl_4_id,
+        self.spl_4_vards,
+        self.spl_4_uzvards
+]
+
+    def __str__(self):
+        return str(self.nosaukums)
+    
+    def get_all(self):
+        return [
+                self.nosaukums,
+
+                self.kaptaina_id,
+                self.kapt_vards ,
+                self.kapt_uzvards,
+
+                self.spl_1_id,
+                self.spl_1_vards,
+                self.spl_1_uzvards,
+
+                self.spl_2_id,
+                self.spl_2_vards,
+                self.spl_2_uzvards,
+
+                self.spl_3_id,
+                self.spl_3_vards,
+                self.spl_3_uzvards,
+
+                self.spl_4_id,
+                self.spl_4_vards,
+                self.spl_4_uzvards,
+
+                self.trn_nosaukums
+                ]
+
+
+# class turnirs:
+    
+#     def __init__(self, nosaukums, kommanda_1, kommanda_2, kommanda_3, kommanda_4, kommanda_5, kommanda_6, kommanda_7, kommanda_8):
+#         self.nosaukums = nosaukums
+
+#         self.kmd_1 = kommanda_1
+#         self.kmd_2 = kommanda_2
+#         self.kmd_3 = kommanda_3
+#         self.kmd_4 = kommanda_4
+#         self.kmd_5 = kommanda_5
+#         self.kmd_6 = kommanda_6
+#         self.kmd_7 = kommanda_7
+#         self.kmd_8 = kommanda_8
+    
+#     def __str__(self):
+#         return self.nosaukums
+    
+#     def get_kommandas(self):
+#         return [
+#             self.kmd_1,
+#             self.kmd_2,
+#             self.kmd_3,
+#             self.kmd_4,
+#             self.kmd_5,
+#             self.kmd_6,
+#             self.kmd_7,
+#             self.kmd_8,
+#         ]
+    
+class tabula:
+
+    def __init__(self, datu_links, turnira_nosaukums):
+
+        self.trn_nosaukums = turnira_nosaukums
+        self.conn = sqlite3.connect(datu_links)
+
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS speletaji(
+                id INTEGER,
+                vards TEXT,
+                uzvards TEXT,
+                steam_vards TEXT,
+                kommandas_id INTEGER,
+                PRIMARY KEY("id"),
+                FOREIGN KEY (kommandas_id) REFERENCES kommandas (id)
+                ) """)
+
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS kommandas(
+                id INTEGER,
+                nosaukums TEXT,
+                kapteina_steam_vards TEXT,
+                turnirs_id TEXT,
+                PRIMARY KEY("id"),
+                FOREIGN KEY (turnirs_id) REFERENCES turniri (id)
+                )""")
+
+
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS turnirs(
+                id INTEGER,
+                nosaukums TEXT,
+                pirma_vieta TEXT,
+                otra_vieta TEXT,
+                tresa_vieta TEXT,
+                PRIMARY KEY("id")
+                )""")
+        
+        
+        #self.conn.execute("INSERT INTO turnirs (nosaukums) VALUES (?)",(turnira_nosaukums,))
+        self.conn.commit()
+
+        turnira_id =self.conn.execute(f"SELECT id FROM turnirs WHERE nosaukums = ?",(turnira_nosaukums,))
+        turnira_id_pag = turnira_id.fetchall()
+        self.trn_id = turnira_id_pag[0][0]
+        print(self.trn_id)
+
+    def ierakstit_kommandu(self, kommanda_viss):
+
+        self.conn.execute("INSERT INTO kommandas (nosaukums, kapteina_steam_vards, turnirs_id) VALUES (?,?,?)",(kommanda_viss[0],kommanda_viss[1],self.trn_id))
+        cursor = self.conn.execute("SELECT id FROM kommandas WHERE nosaukums = ?",(kommanda_viss[0],))
+        kmd_pagaidu = cursor.fetchone()
+        kommandas_id = kmd_pagaidu[0]
+
+        self.conn.execute("INSERT INTO speletaji (vards, uzvards, steam_vards, kommandas_id) VALUES (?,?,?,?)",(kommanda_viss[2],kommanda_viss[3],kommanda_viss[1],kommandas_id))
+        self.conn.execute("INSERT INTO speletaji (vards, uzvards, steam_vards, kommandas_id) VALUES (?,?,?,?)",(kommanda_viss[5],kommanda_viss[6],kommanda_viss[4],kommandas_id))
+        self.conn.execute("INSERT INTO speletaji (vards, uzvards, steam_vards, kommandas_id) VALUES (?,?,?,?)",(kommanda_viss[8],kommanda_viss[9],kommanda_viss[7],kommandas_id))
+        self.conn.execute("INSERT INTO speletaji (vards, uzvards, steam_vards, kommandas_id) VALUES (?,?,?,?)",(kommanda_viss[11],kommanda_viss[12],kommanda_viss[10],kommandas_id))
+        self.conn.execute("INSERT INTO speletaji (vards, uzvards, steam_vards, kommandas_id) VALUES (?,?,?,?)",(kommanda_viss[14],kommanda_viss[15],kommanda_viss[13],kommandas_id))
+
+        self.conn.commit()
+
+    def izvadit_dalibniekus(self):
+        dalibnieki = self.conn.execute("SELECT * FROM kommandas WHERE turnirs_id = ?",(self.trn_id,))
+        cursor = dalibnieki.fetchall()
+        return cursor[0]
+    
+def main():
+#     kommanda1 = kommanda("Gurķīši",1111,"Kristaps","Rūtainis",2222,"Mārtiņš","Zaķis",3333,"Evards","Gleizds",4444,"Kristijāns","Ligeris",5555,"Jānis","Kazerovskis","Milzu turnīrs")
+#     print(kommanda1.get_all())
+
+#     turnirs_1 = tabula("milzu_turnirs.db","Milzu turnīrs")
+#     #turnirs_1.ierakstit_kommandu(kommanda1.get_all())
+#     print(turnirs_1.izvadit_dalibniekus())
+    test = "test"
+    turnirs_2 = tabula(f"{test}.db","testa")
+if __name__=="__main__":
+    main()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from datubaze import *
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
@@ -16,18 +19,59 @@ def rules():
 
 @app.route('/tournaments')
 def tournaments():
-    tournament = {
-        "name": "ESL Pro League Season - 21 ",
-        "dates": "30.04 - 12.05 ",
-        "prize": "1 000 000 $"
-    }
-    return render_template('tournaments.html', tournament=tournament)
 
-@app.route('/registracija')
-def registracija():
-    registracija = {
+    return render_template('tournaments.html')
 
-    }
-    return render_template('registracija.html')
+@app.route('/registracija/ESL')
+def registracija_ESL():
+    nosaukums = "ESL Pro League Season - 21"
+    global turnira_sais
+    global turnira_nosaukums
+    turnira_nosaukums = nosaukums
+    turnira_sais = "ESL"
+    return render_template('registracija.html', nosaukums_trn=nosaukums)
+    
+
+@app.route('/registracija/IEM', methods=['GET','POST'])
+def registracija_IEM():
+    nosaukums = "IEM Dallas"
+    global turnira_sais
+    global turnira_nosaukums
+    turnira_nosaukums = nosaukums
+    turnira_sais = "IEM"
+    return render_template('registracija.html', nosaukums_trn=nosaukums)
+
+@app.route('/registracija/BLASTtv', methods=['GET','POST'])
+def registracija_BLASTtv():
+    nosaukums = "BLAST.tv Austin Major 2025"
+    global turnira_sais
+    global turnira_nosaukums
+    turnira_nosaukums = nosaukums
+    turnira_sais = "BLASTtv"
+    return render_template('registracija.html', nosaukums_trn=nosaukums)
+
+@app.route('/pieteikts', methods=['GET','POST'])
+def pieteikts():
+    
+    
+    global turnira_nosaukums
+    turnira_nosaukums = "ESL Pro League Season - 21"
+    trn_nosaukums = turnira_nosaukums
+    global turnira_sais
+    turnira_sais = "ESL"
+    pieteikta_kmd = kommanda(request.form.get("kmd_nosaukums"),
+                             request.form.get("kpt_steam_vards"),request.form.get('kpt_vards'),request.form.get('kpt_uzvards'),
+                             request.form.get("kpt_steam_vards"),request.form.get('kpt_vards'),request.form.get('kpt_uzvards'),
+                             request.form.get("kpt_steam_vards"),request.form.get('kpt_vards'),request.form.get('kpt_uzvards'),
+                             request.form.get("kpt_steam_vards"),request.form.get('kpt_vards'),request.form.get('kpt_uzvards'),
+                             request.form.get("kpt_steam_vards"),request.form.get('kpt_vards'),request.form.get('kpt_uzvards'),
+                             turnira_sais
+                             )
+    turnirs = tabula(f"static/{turnira_sais}.db",turnira_sais)
+    turnirs.ierakstit_kommandu(pieteikta_kmd.get_all())
+
+    return render_template('pieteikts.html', kmd_nosaukums=pieteikta_kmd.__str__(), trn_nosaukums=trn_nosaukums )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
